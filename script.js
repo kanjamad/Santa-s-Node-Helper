fe3abe6eea1dbe204932b6b99a8d1a9410732a8f
Kanjamad Bosel <npor25@yahoo.com>
2019-02-18 20:51:13 -0800

    fun to think about how to structure my code

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const fs = require('fs');
 
 function question1(){
     fs.readFile('./santa.txt', (err, data) => {
+        console.time('santa-time');
         const directions = data.toString();
         const directionsArray = directions.split('');
         const answer = directionsArray.reduce((acc, currentValue) => {
@@ -14,6 +15,7 @@ function question1(){
                 return acc -= 1
             }
         }, 0)
+        console.timeEnd('santa-time');
         console.log('floor:', answer);
     })
 }
