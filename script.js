5a3291febb47b4494a356d4fba3270309c5cdc2c
Kanjamad Bosel <npor25@yahoo.com>
2019-02-18 23:10:38 -0800

    answer question part 2

--- script.js
+++ script.js
@@ -2,10 +2,9 @@ const fs = require('fs');
 // 1 what floor do the instructions take Santa?
 // (--> should go UP 1 floor
 // )--> should go Down 1 floor
-
 function question1(){
     fs.readFile('./santa.txt', (err, data) => {
-        console.time('santa-time');
+        console.time('Q1 = santa-time');
         const directions = data.toString();
         const directionsArray = directions.split('');
         const answer = directionsArray.reduce((acc, currentValue) => {
@@ -15,12 +14,33 @@ function question1(){
                 return acc -= 1
             }
         }, 0)
-        console.timeEnd('santa-time');
+        console.timeEnd('Q1 = santa-time');
         console.log('floor:', answer);
     })
 }
 
 question1();
 
-
 // 2 when does santa first enter the basement?
+function question2(){
+    fs.readFile('./santa.txt', (err, data) => {
+        console.time('Q2 = santa-time');
+        const directions = data.toString();
+        const directionsArray = directions.split('');
+        let accumulator = 0
+        let counter = 0
+        const answer = directionsArray.some((currentItem) => {
+            if (currentItem== '('){
+                accumulator += 1
+            } else if (currentItem === ')'){
+                accumulator -= 1
+            }
+            counter ++
+            return accumulator < 0;
+        })
+        console.timeEnd('Q2 = santa-time');
+        console.log('basement entered at:', counter);
+    })
+}
+
+question2()
